ceb490dc370f20777a77d81c2c0cab60cd3722cd
unknown <irfanulhaq228@gmail.com>
2024-03-20 12:56:29 +0500

    version 1.2

--- Controllers/HomeForRentController.js
+++ Controllers/HomeForRentController.js
@@ -65,12 +65,29 @@ const updateRentalHomeById = async(req, res) => {
         console.log(error);
         return res.status(500).json({ message: "Network Error" });
     }
-}
+};
+
+const approvedRentalHomes = async(req, res) => {
+    try{
+        const data = await homeForRentModel.find({ status: true }).populate({
+            path: 'agent',
+            select: '_id name email address status'
+        });
+        if(data?.length === 0){
+            return res.status(400).json({ message: "No Data Found For Rent" })
+        }
+        return res.status(200).json({ message: data });
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({ message: "Network Error" });
+    }
+};
 
 module.exports = { 
     getHomesForRent, 
     createHomesForRent, 
     getRentalHomeById, 
     deleteRentalHomeById, 
-    updateRentalHomeById 
+    updateRentalHomeById,
+    approvedRentalHomes
 }
--- Routes/HomeForRentRoute.js
+++ Routes/HomeForRentRoute.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const { getHomesForRent, createHomesForRent, getRentalHomeById, deleteRentalHomeById } = require("../Controllers/HomeForRentController");
+const { getHomesForRent, createHomesForRent, getRentalHomeById, deleteRentalHomeById, approvedRentalHomes } = require("../Controllers/HomeForRentController");
 const { upload } = require("../Multer/Multer");
 
 const HomeForRentRouter = express.Router();
 
-HomeForRentRouter.get("/", getHomesForRent);
 HomeForRentRouter.post("/create", upload.fields([ { name: 'images' }, { name: "video" } ]), createHomesForRent);
+
+HomeForRentRouter.get("/", getHomesForRent);
+HomeForRentRouter.get("/approved", approvedRentalHomes);
+
 HomeForRentRouter.get("/:id", getRentalHomeById);
 HomeForRentRouter.delete("/:id", deleteRentalHomeById);
 
